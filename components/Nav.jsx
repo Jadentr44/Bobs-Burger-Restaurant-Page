f36cd68e58baca6c6dfa7b338149a462dd716183
Jaden <jadentr.44@gmail.com>
2022-11-09 11:34:28 -0600

    added animation to mobile menu toggle

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import Image from "next/image";
+import {motion,useCycle} from 'framer-motion'
 import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import Link from 'next/link'
+
+const Path = (props) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    stroke="currentColor"
+    strokeLinecap="round"
+    {...props}
+  />
+);
+
+const MenuButton = ({ onClick, isOpen }) => {
+  return (
+    <motion.button
+      className="menu-button"
+      onClick={onClick}
+      animate={isOpen ? "open" : "closed"}
+      initial={false}
+    >
+      <svg
+        width="70"
+        height="60"
+        style={{ margin: "4px 0 0 2px" }}
+        viewBox="0 0 23 23"
+      >
+        <Path
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" }
+          }}
+        />
+        <Path
+          d="M 2 9.423 L 20 9.423"
+          variants={{
+            closed: { opacity: 1 },
+            open: { opacity: 0 }
+          }}
+          transition={{ duration: 0.1 }}
+        />
+        <Path
+          variants={{
+            closed: { d: "M 2 16.346 L 20 16.346" },
+            open: { d: "M 3 2.5 L 17 16.346" }
+          }}
+        />
+      </svg>
+    </motion.button>
+  );
+};
+
 export default function Nav() {
+  const [isOpen, toggleDropdown] = useCycle(false, true);
+  
   return (
     <>
       {/* computer nav */}
@@ -44,7 +97,15 @@ export default function Nav() {
         </div>
       </nav>
 
-      <div className="lg:hidden block"></div>
+      <div className="lg:hidden block">
+        
+    <div className="absolute right-5 top-5">
+
+       <MenuButton onClick={toggleDropdown} isOpen={isOpen} />
+    </div>
+        
+    
+      </div>
     </>
   );
 }
